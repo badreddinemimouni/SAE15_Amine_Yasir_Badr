b247023944c133316ebcdd5de47970f5007a87ed
BADHTW <badr_mim@outlook.fr>
2023-01-23 11:29:25 +0100

    programme graphiques

--- mkrd.py
+++ mkrd.py
@@ -5,11 +5,17 @@ f2=open("FR_MTP_CORU.txt","r",encoding='utf-8')
 
 lignes=f2.readlines()
 tab=[]
+tab2=[]
 for ligne in lignes:
 	ligne=ligne.replace('\n','')
 	donnees=ligne.split(',')
 	tab.append(float(donnees[0]))
-for i in range(len(tab)):
-    plt.plot(tab[i])
-    plt.ylabel('Label 1')
-    plt.show()
+for ligne2 in lignes:
+    ligne2=ligne2.replace('\n','')
+    donnees2=ligne2.split(',')
+    tab2.append(donnees[1])
+
+plt.plot(tab)
+plt.ylabel(tab2)
+plt.xlabel(tab2)
+plt.show()
